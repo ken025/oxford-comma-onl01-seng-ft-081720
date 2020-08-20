8f031d78a34360d3a7bff6a40ff023eb6db2b032
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-20 01:04:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,8 +10,13 @@ def oxford_comma(array)
   array[2] = "and #{array[2]}"
   array.join(", ")
   
+<<<<<<< HEAD
   elsif array.length > 3
   array[-1] = "and #{array[-1]}"
   array.join(", ")
+=======
+  elsif array.length < 3
+   array[0..-2].join(', ') + ", and " + array[-1]
+>>>>>>> cfb51192f5af928c027e675e27b012ea1980d7bb
 end
 end 
